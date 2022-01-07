c7c863dcd5e7d7350cddbbddcf16ffc2d76f7284
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-01-07 19:10:16 +0300

    Objects and object destructuring

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 "use strict"
 
-function first() {
-    setTimeout(function(){
-        console.log(1)
-    }, 500)
-}
-function second() {
-    console.log(2)
-}
-first()
-second()
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function () {
+        console.log('test!')
+    }
+};
+console.log(options['colors']['border'])
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback()
-}
+delete options.name
 
-function done() {
-    console.log('Я прошел этот урок!')
+console.log(options)
+let counter = 0;
+for (let key in options) {
+    if (typeof options[key] === "object") {
+        for (let i in options[key]) {
+            console.log(`Свойство ${key} имеет значение ${options[key][i]}`)
+            counter++
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`)
+        counter++
+    }
 }
+console.log(counter)
+console.log(Object.keys(options).length)
+console.log(options.makeTest())
 
-learnJS('JavaScript', done)
+const {border, bg} = options.colors;
+console.log(border)
