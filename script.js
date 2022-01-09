d5bb7b985a4d32f9bd7c7678c158637c75df39ad
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-01-09 12:39:25 +0300

    Passing by reference or by value

--- script.js
+++ script.js
@@ -1,147 +1,92 @@
 "use strict"
 
-// const str = prompt('', '');
-// const products = str.split(',')
-// products.sort();
-// console.log(products.join('!'))
-// let arr = [122, 31, 12, 5, 34 ,64 ,322];
-// let sorti = (a, b) => a - b
-// console.log(arr.sort(sorti))
-let arr = [1, 2, 3, 4, 5];
-arr.push(14)
-console.log(arr)
+let a = 5;
+let b = a;
 
-let arr = [1, 2, 3, 4, 5];
-arr.pop()
-console.log(arr)
+b = b + 5;
 
-let arr = [1, 2, 3, 4, 5];
-arr.shift()
-console.log(arr)
+console.log(a)
 
-let arr = [1, 2, 3, 4, 5];
-arr.unshift(0)
-console.log(arr)
-
-let arr = [1, 2, 3, 4, 5];
-for (let val of arr) {
-    console.log(val)
+const obj = {
+    a: 5,
+    b: 1
 }
-1
-2
-3
-4
-5
+const copy = obj; // КОПИРУЯ ОБЪЕКТ, КОПИРУЕШЬ ССЫЛКУ
 
-let arr = [1, 2, 3, 4, 5];
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i])
+
+copy.a = 10;
+console.log(obj)
+console.log(copy)
+                        //ПОВЕРХНОСТНАЯ КОПИЯ ОБЪЕКТА
+function copy(mainObj) {
+    let objCopy = {}
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key]
+    }
+    return objCopy;
 }
-1
-2
-3
-4
-5
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
 
-let arr = [1, 2, 3, 4, 5];
-arr.splice(1, 2, 'two, three')
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+console.log(newNumbers)  // { a: 10, b: 5, c: { x: 7, y: 4 } }
+console.log(numbers)     // { a: 2, b: 5, c: { x: 7, y: 4 } }
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const add = {
+    d: 17,
+    e: 20
+};
+let arr = Object.assign(numbers, add)
 console.log(arr)
-    [ 1, 'two, three', 4, 5 ]
 
-let arr = [1, 2, 3, 4, 5];
-let arr2 = arr.slice(0, 3)
-console.log(arr2)
-    [ 1, 2, 3 ]
+let app = Object.assign({}, add)
+app.d = 24;
+console.log(add)
+console.log(app)
 
-let arr = [1, 2, 3, 4, 5];
-let arr2 = arr.concat('new element')
-console.log(arr2)
-    [ 1, 2, 3, 4, 5, 'new element' ]
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
 
-let arr = [1, 2, 3, 4, 5];
-let arr2 = arr.forEach((item, id, arr) => {
-    console.log(`${item} имеет позицию ${id} в массиве ${arr}`)
-})
-console.log(arr2)
-1 имеет позицию 0 в массиве 1,2,3,4,5
-2 имеет позицию 1 в массиве 1,2,3,4,5
-3 имеет позицию 2 в массиве 1,2,3,4,5
-4 имеет позицию 3 в массиве 1,2,3,4,5
-5 имеет позицию 4 в массиве 1,2,3,4,5
+                  Spread Operator
+const video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'blogger'],
+    internet = [...video,...blogs, 'vk', 'facebook'];
+console.log(internet)
+           Spread Operator with function
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+const num = [2, 5, 7];
 
-let arr = [1, 2, 3, 4, 5];
-let lol = arr.indexOf(4, 0);
-console.log(lol)
-  3
+log(...num)
 
-let arr = [1, 2, 3, 4, 5];
-let lol = arr.includes(4, 0);
-console.log(lol)
-  true
+const array = ['a', 'b'];
 
-let users = [
-    {id: 1, name: "Вася"},
-    {id: 2, name: "Петя"},
-    {id: 3, name: "Маша"}
-];
-let lol = users.find(item => item.name == 'Петя')
-console.log(lol)
-{ id: 2, name: 'Петя' }
+const newArray = [...array]
 
-let arr = [1, 2, 3, 4, 5, 'Node.js'];
-let lol = arr.findIndex(item => item == 'Node.js');
-console.log(lol)
-5
-
-let users = [
-    {id: 1, name: "Вася"},
-    {id: 2, name: "Петя"},
-    {id: 3, name: "Маша"}
-];
-let lol = users.filter(item => item.id > 1)
-console.log(lol)
-    [ { id: 2, name: 'Петя' }, { id: 3, name: 'Маша' } ]
-
-let arr = [1, 2, 3, 4, 5, 'Node.js'];
-let lol = arr.map(item => item * 2);
-console.log(lol)
-    [ 2, 4, 6, 8, 10, NaN ]
-
-let arr = [1, 2, 3, 4, 5, 44, 23, 31 ,12 ,332];
-let sorti = (a, b) => a - b;
-let arr2 = arr.sort(sorti)
-console.log(arr2)
-    [
-    1,  2,  3,  4,   5,
-        12, 23, 31, 44, 332
-    ]
-
-let arr = [1, 2, 3, 4, 5, 44, 23, 31 ,12 ,332];
-let arr2 = arr.reverse()
-console.log(arr2)
-    [
-    332, 12, 31, 23, 44,
-        5,  4,  3,  2,  1
-    ]
-
-let arr = 'Features Careers Blog Pricing Jam';
-console.log(arr.split(' '))
-    [ 'Features', 'Careers', 'Blog', 'Pricing', 'Jam' ]
-
-let arr = [ 'Features', 'Careers', 'Blog', 'Pricing', 'Jam' ];
-console.log(arr.join(', '))
-   Features, Careers, Blog, Pricing, Jam
-
-let arr = [1, 2, 3, 4, 5];
-let arr2 = arr.reduce((sum, current) => sum + current)
-console.log(arr2)
-  15
-
-let arr = [1, 2, 3, 4, 5];
-let arr2 = arr.reduceRight((sum, current) => sum + current)
-console.log(arr2)
- 15
-
-let arr = [1, 2, 3, 4, 5];
-console.log(Array.isArray(arr))
-  true
+const q = {
+    one: 1,
+    two: 2
+}
+const newObj = {...q}
+console.log(newObj)
