347d4de3cca8916241afd4ba967b1bbdeea60df9
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-01-10 17:15:15 +0300

    Object Oriented Programming

--- script.js
+++ script.js
@@ -1,92 +1,28 @@
 "use strict"
 
-let a = 5;
-let b = a;
+let str = "some";
+let strObj = new String(str);
 
-b = b + 5;
+console.log( typeof str)
+console.log( typeof strObj)
 
-console.log(a)
+console.dir([1,2, 3])
 
-const obj = {
-    a: 5,
-    b: 1
-}
-const copy = obj; // КОПИРУЯ ОБЪЕКТ, КОПИРУЕШЬ ССЫЛКУ
-
-
-copy.a = 10;
-console.log(obj)
-console.log(copy)
-                        //ПОВЕРХНОСТНАЯ КОПИЯ ОБЪЕКТА
-function copy(mainObj) {
-    let objCopy = {}
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key]
-    }
-    return objCopy;
-}
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHi: function (){
+        console.log('Hello!')
     }
 };
+const jonh = Object.create(soldier);
 
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-console.log(newNumbers)  // { a: 10, b: 5, c: { x: 7, y: 4 } }
-console.log(numbers)     // { a: 2, b: 5, c: { x: 7, y: 4 } }
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
+const jonh = {
+    health: 100
 };
 
-const add = {
-    d: 17,
-    e: 20
-};
-let arr = Object.assign(numbers, add)
-console.log(arr)
+jonh.__proto__ = soldier;
+Object.setPrototypeOf(jonh, soldier)
+console.log(jonh.armor);
+jonh.sayHi()
 
-let app = Object.assign({}, add)
-app.d = 24;
-console.log(add)
-console.log(app)
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-                  Spread Operator
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejournal', 'blogger'],
-    internet = [...video,...blogs, 'vk', 'facebook'];
-console.log(internet)
-           Spread Operator with function
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-const num = [2, 5, 7];
-
-log(...num)
-
-const array = ['a', 'b'];
-
-const newArray = [...array]
-
-const q = {
-    one: 1,
-    two: 2
-}
-const newObj = {...q}
-console.log(newObj)
