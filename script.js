c95faaa0444aa603abb7644b173a0527db71bc55
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-01-13 21:39:04 +0300

    Actions with elements on the site page

--- script.js
+++ script.js
@@ -1,25 +1,77 @@
 'use strict';
 
-1. document.getElementById(""); - получить элемент по id
+// Действия с элементами на странице
 const box = document.getElementById('box');
-console.log(box);
-
-2. document.getElementsByTagName(""); - получить элементы по тегу
 const btns = document.getElementsByTagName('button');
-console.log(btns)
+const circles = document.getElementsByClassName('circle');
+const hearts = document.querySelectorAll('.heart');
+const oneHeart = document.querySelector('.heart');
 
-let btns = document.getElementsByTagName("")[1] - вернет 2-й элемент
-или
-console.log(btns[1])
-3.  const circles = document.getElementsByClassName('circle'); -  получить элемент по Классу
-console.log(circles)
 
-4. const hearts = document.querySelectorAll('.heart'); - получить элемент по селектору css
+1) box.style.backgroundColor = 'blue';
+Чтобы изменить стили объектов, надо использовать св-во style
 
-hearts.forEach(item => {
-    console.log(item)
+
+2) circles[0].style.backgroundColor = 'red';
+Чтобы стили конкретного элемента псевдамассива, надо обратиться к элементу по его индексу
+
+3)box.style.cssText = 'background-color: blue; width: 500px';
+Чтобы задать СРАЗУ НЕСКОЛЬКО стилей объекту, надо использовать св-во cssText
+
+4) for (let i = 0; i < hearts.length; i++) {
+    hearts[i].style.backgroundColor = 'blue';
+}
+Перебрать элементы массива ЦИКЛОМ и каждому поменять стили
+
+5) hearts.forEach(item => {
+    item.style.backgroundColor = 'green';
 })
-Поддерживает метод forEach()
+Перебрать элементы массива МЕТОДОМ forEach() и каждому поменять стили
 
-5. const oneHeart = document.querySelector('.heart'); - получить один элемент со страницы(первый искомый)
-console.log(oneHeart)
+6) const div = document.createElement('div');
+Создать элемент c тегом, указанным в аргументе
+Тег создается только внутри файла JS
+
+7) const text = document.createTextNode('Тут был я');
+Создать текстовый узел без оболочки тега
+
+8) div.classList.add('black');
+Добавить класс black элементу div
+
+9) document.body.append(div);
+Добавить элемент div внутрь элемента body
+
+10) document.querySelector('.wrapper').append(div);
+Получить элемент с классом .wrapper и добавить туда div В КОНЕЦ
+
+11) wrapper.prepend(div);
+Добавить элемент div в НАЧАЛО элемента wrapper
+
+12) hearts[0].before(div)
+Добавить элемент div ПЕРЕД элементом hearts[0]
+
+13) hearts[0].after(div)
+Добавить элемент div ПОСЛЕ элемента hearts[0]
+
+14) circles[0].remove();
+Удалить элемент circles[0] со страницы
+
+15) hearts[0].replaceWith(circles[0]);
+Заменить элемент  hearts[0] на элемент circles[0]
+
+   // РЕДАКАТИРОВАНИЕ ЭЛЕМЕНТОВ
+
+1) div.innerHTML = 'Hello World!';
+Вставить текст 'Hello World!' в элемент div
+ МОЖНО ВСТАВЛЯТЬ В ТЕГАХ
+div.innerHTML = '<h1>Hello World!</h1>';
+
+2) div.textContent = 'Hello';
+Вставить только текст 'Hello' в элемент div, БЕЗ ТЕГОВ
+
+3) div.insertAdjacentHTML('afterend', '<h2>helllo</h2>');
+beforebegin - вставить html перед элементом div
+afterbegin - вставить html в начало элемента div
+beforeend - вставить html в конец элемента div
+afterend - вставить html после элемента div
+
