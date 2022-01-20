0275978edf409a08d6bed6640f7e02d468373d2d
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-01-20 20:15:37 +0300

    Events on mobile devices

--- script.js
+++ script.js
@@ -1,52 +1,44 @@
 'use strict';
 
-Навигация по DOM-элементам, data-атрибуты, преимущества for/of
+// СОБЫТИЯ НА МОБИЛЬНОМ БРАУЗЕРЕ
 
-console.dir(document.body.childNodes)
-Получить дочерние узлы body
+// touchstart - событие срабатывает при касании к элементу
+// touchmove - при движении пальца по элементу
+// touchend - при отрыве пальца от элемента
+// touchenter - при ведении пальца по экрану и попадании на элемент с событием
+// touchleave - при прекращении ведения пальца по событию, но при ведении дальше по экрану
+// touchcancel - точка соприкосновения не регистрируется на поверхности( палец выходит за пределы)
 
-console.dir(document.body.firstChild)
-Получить первый дочерний узел
-console.dir(document.body.lastChild)
-Получить последний дочерний узел
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        console.log('Start!');
+        console.log(e.changedTouches);
+    });
+});
+//
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        console.log(e.targetTouches[0].pageX);
+    });
+});
+//
+// window.addEventListener('DOMContentLoaded', () => {
+//     const box = document.querySelector('.box');
+//     box.addEventListener('touchend', (e) => {
+//         e.preventDefault();
+//         console.log('End!');
+//     });
+// });
 
-console.log(document.querySelector('#current').parentNode);
-Получить родительский узел
+// touches - выдает список всех пальцев, которые сейчас взаимодействуют с экраном
+// targetTouches - пальцы, которые взаимодействуют именно с элмементом
+// changedTouches - пальцы, которые участвуют в текущем событии(совершили событие)
+// e.targetTouches[0].pageX - выведет координаты области нажатия
 
- console.log(document.querySelector('#current').parentNode.parentNode);
-Получить родителя родителя
-
-console.log(document.querySelector('[data-current="3"]'));
-Получить элемент с  data-атрибутом
-
-console.log(document.querySelector('[data-current="3"]').nextSibling);
-Получить следующего соседа
-
-console.log(document.querySelector('[data-current="3"]').previousSibling);
-Получить предыдущего соседа
-
-console.log(document.querySelector('[data-current="3"]').nextElementSibling);
-Получить следующий элемент
-
-console.log(document.querySelector('[data-current="3"]').previousElementSibling);
-Получить предыдущий элемент
-
-console.log(document.querySelector('#current').parentElement);
-Получить родительский элемент
-
-console.log(document.body.firstElementChild)
-Получить первый дочерний ЭЛЕМЕНТ
-
-console.log(document.body.lastElementChild)
-Получить последний дочерний ЭЛЕМЕНТ
-
- for (let node of document.body.childNodes) {
-     if (node.nodeName == '#text') {
-         continue;
-     }
-     console.log(node)
- }
- Перебрать дочерние узлы body и вывести только Элементы
 
 
 
