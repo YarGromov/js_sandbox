b227572d8ce7936eebaf7cf945542d755383c62b
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-01-30 19:35:24 +0300

    ClassList and Event Delegation

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
 'use strict';
 
-// Async, defer, динамические скрипты
-
-const p = document.querySelectorAll('p')
-console.log(p)
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
 
+// console.log(btns[0].classList.length);
+//Выведет КОЛИЧЕСТВО КЛАССОВ у элемента btns[0]
 
-function loadScript(src) {
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    document.body.append(script);
-}
-loadScript('test.js')
-loadScript('some.js')
+// console.log(btns[0].classList.item(0));
+//Позволяет получить class под индексом 0 элемента btns[0]
+
+// console.log(btns[1].classList.add('red'));
+//Добавить класс 'red' элементу 'btns[1]'.Можно добавить несколько классов через запятую
+
+// console.log(btns[0].classList.remove('blue'));
+//Удалить класс 'blue'. Можно удалить несколько классов через запятую
+
+// console.log(btns[0].classList.toggle('blue'));
+//Добавит класс, если его не было. Удалит класс, если он был
+
+// if (btns[1].classList.contains('red')) {
+//     console.log('looool red')
+// }
+// Позволяет проверить наличие определенного класса у опред. элемента
+
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red')
+    // }
+    btns[1].classList.toggle('red');
+})
+// console.log(btns[0].className)
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.matches('button.red')) {
+        console.log('Hello!!')
+    }
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello!')
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
+
 
 
 
