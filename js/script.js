0d960eb58bb2731c33849b1d127be2db8bd3adca
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-06-28 12:29:10 +0200

    УСЛОВИЕ

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
-class Rectangle {
-  constructor(width, height){
-    this.width = width;
-    this.height = height;
-  }
+// УСЛОВИЯ if - else
 
-  calcArea(){
-    return this.width * this.height;
-  }
+const num = 50; 
+
+if (num < 49) {
+  console.log('Error!')
+} else if(num > 100) {
+  console.log('Слишком много')
+} else {
+  console.log('Ok!')
 }
 
- const square = new Rectangle(11, 10);
+// ТЕРНАРНЫЙ ОПЕРАТОР
 
- console.log(square.calcArea())
+const num2 = 51; 
+
+(num2 === 50) ? console.log('Ok!') : console.log('Error!')
+
+// SWITCH  проверка на строгое соответствие 
+
+const num3 = 153;
+
+switch(num3) {
+  case 51:
+    console.log('Мало');
+    break;
+  case 52:
+    console.log('Мало');
+    break;
+  case 53:
+    console.log('В точку');
+    break;
+  case 54:
+    console.log('Много');
+    break;
+  default:
+    console.log('Не в этот раз!');
+    break;
+}
+
