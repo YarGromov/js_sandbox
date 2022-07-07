d8b740f40c07765a7acfb7f706d5a6201b707901
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-07-07 20:10:15 +0200

    Объекты и их деструктуризация

--- js/script.js
+++ js/script.js
@@ -1,27 +1,47 @@
-//callback - функции
+// Объекты, деструктуризация объектов (ES6)
 
-function first() {
-  setTimeout(function(){
-    console.log(1);
-  }, 500);
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red'
+  },
+  makeTest: function(){
+    console.log('Test'); 
+  }
+};
+
+console.log(options['colors']['border'])
+// 
+// delete options.name;
+
+// console.log(options)
+
+let counter = 0;
+
+for (let key in options){
+  if(typeof (options[key]) === 'object') {
+    for (let i in options[key]){
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+    } 
+  } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`)
+    counter++;
+  }
 }
+console.log(counter)
 
 
-function second(){
-  console.log(2);
-}
+console.log(Object.keys(options));
+console.log(Object.keys(options).length);
 
-first()
-second()
+options.makeTest()
 
+console.log(options.makeTest())
 
-function learnJS(lang, callback){
-  console.log(`Я учу: ${lang}`);
-  callback();
-}
-
-function done(){
-  console.log('Я прошел этот урок')
-}
-
-learnJS('JavaScript', done);
+// Деструктуризация 
+ 
+const {border, bg} = options.colors;
+console.log(border, bg);
